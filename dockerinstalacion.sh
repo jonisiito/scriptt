02fd9327d1b325ff9159be890be79900b3ece48c
jonisiito <97846016+jonisiito@users.noreply.github.com>
2023-03-03 12:31:55 +0100

    Update dockerinstalacion.sh

--- dockerinstalacion.sh
+++ dockerinstalacion.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-apt update 
-apt -y upgrade
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update
-apt install docker-ce docker-ce-cli containerd.io
+apt install git
+apt-get install ca-certificates
+apt-get install curl
+apt-get install gnupg
+
+apt-get install lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+adduser admin docker
